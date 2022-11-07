e45e01951003f8df03915c3684ac0abd477afa08
Bruno Martins <bgcngm@gmail.com>
2022-11-07 12:41:18 -0300

    veux: Build livedisplay 2.0

--- device.mk
+++ device.mk
@@ -256,6 +256,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm    
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
